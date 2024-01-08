368a36f8544ce3904876bd5d6ae9e41dae30f482
0512br <asa.pf1205@gmail.com>
2024-01-08 16:40:09 +0900

    Done two-sum

---
+++ Easy/two-sum.py
@@ -0,0 +1,16 @@
+# first answer
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            for j in range(i+1, len(nums)):
+                if diff == nums[j]:
+                    return[i, j]
+
+# refactoring
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
