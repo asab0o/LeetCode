934ff08f0371162895af98e0495df079a93d54af
0512br <asa.pf1205@gmail.com>
2024-01-08 17:56:51 +0900

    add another solutions

--- Easy/best-time-to-buy-and-sell-stock.py
+++ Easy/best-time-to-buy-and-sell-stock.py
@@ -18,3 +18,31 @@ class Solution:
             max_diff = max(max_diff, price - min_price)
             min_price = min(min_price, price)
         return max_diff
+
+# use float method for init
+class Solution:
+    def maxProfit(self, prices):
+        min_price = float("inf")
+        max_profit = 0
+
+        for num in prices:
+            min_price = min(min_price, num)
+            max_profit = max(max_profit, num - min_price)
+        
+        return max_profit
+
+# right と left のポインタを使用
+class Solution:
+    def maxProfit(self,prices):
+        left, right = 0, 1
+        max_profit = 0
+        while right < len(prices):
+            print(left, right)
+            if prices[left] < prices[right]:
+                current_profit = prices[right] - prices[left]
+                max_profit =max(current_profit, max_profit)
+            else:
+                # 最小値はprices[right]ではないことがわかったため
+                left = right
+            right += 1
+        return max_profit
