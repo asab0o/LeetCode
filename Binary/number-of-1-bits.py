c3aa6162a5fab228a8a6ce50e82faa8a1d21fcde
0512br <asa.pf1205@gmail.com>
2024-02-09 21:26:04 +0900

    解けなかったため、また改めて実施

--- Binary/number-of-1-bits.py
+++ Binary/number-of-1-bits.py
@@ -15,5 +15,16 @@ class Solution:
         count = 0
         while n != 0:
             n &= (n - 1) # もとの数値の1のバイトを消していく
+            # n      :101100
+            # n-1    :101011(nの一番右にある1が0になる)
+            # n & n-1:101000 
             count += 1
+        return count
+
+# 0または1を足していく
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        for char in format(n, 'b'):
+            count += int(char)
         return count
